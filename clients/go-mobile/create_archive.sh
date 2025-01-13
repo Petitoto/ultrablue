d2f613417289645f3181f3acf7be147ac5b68c39
Petitoto <Petitoto@users.noreply.github.com>
2025-01-13 16:13:39 +0100

    android: update to API 21

--- clients/go-mobile/create_archive.sh
+++ clients/go-mobile/create_archive.sh
@@ -6,9 +6,7 @@ go install golang.org/x/mobile/cmd/gomobile@latest
 go mod download golang.org/x/mobile
 go get golang.org/x/mobile/bind
 
-# Force Android API to >= 19 to work around bug with newer SDK:
-# https://github.com/golang/go/issues/52470#issuecomment-1203874724
-gomobile bind -target=android -androidapi 19 -v .
+gomobile bind -target=android -androidapi 21 -v .
 
 TARGET="../Android/ultrablue/app/libs"
 mkdir -p $TARGET
